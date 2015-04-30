afe0b19d9005fce7e55d159c2ddb3c0e00b5e6bc
Gullit Miranda <gullitmiranda@gmail.com>
2015-04-30 06:49:40 -0300

    [Kernel] Adding `newrelic` with app monitor

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ require('source-map-support').install();
 
 require('dotenv').load({ silent: true });
 
+if (process.env.NEW_RELIC_LICENSE_KEY && process.env.NEW_RELIC_LOG) {
+  require('newrelic')
+}
+
 var Server = require('./lib/src/server.js');
 var port = process.env.HTTP_PORT || process.env.PORT;
 
